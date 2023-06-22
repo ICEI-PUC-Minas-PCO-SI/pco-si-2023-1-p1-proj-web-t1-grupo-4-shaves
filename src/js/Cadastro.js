6947cf2d191a1213f38e158c8cee858b0845881d
GuilhermeSimaos <guilhermesimaogh@outlook.com>
2023-06-21 23:44:37 -0300

    Reworked Login Page for mobile

--- src/js/Cadastro.js
+++ src/js/Cadastro.js
@@ -1,5 +1,4 @@
 import Swal from 'sweetalert2';
-import './ModuloConexao'
 
 // Mostrar Senha logic
 var mostrarSenhaCheckbox = document.getElementById("mostrarSenhaCheckbox");
@@ -13,31 +12,5 @@ mostrarSenhaCheckbox.addEventListener("change", function () {
   } else {
     inputSenha.type = "password";
     inputConfSenha.type = "password";
-  }
-});
-
-// Cadastrar Conta
-var inputNome = document.getElementById("inputNome");
-var criarContaBtn = document.getElementById('criarContaBtn');
-
-criarContaBtn.addEventListener('submit', function (Event) {
-  Event.preventDefault();
-
-  // Verificar se todos os campos estão preenchidos
-  if (
-    inputNome !== 0
-  ) {
-    Swal.fire({
-      icon: 'success',
-      title: 'Conta Criada!',
-      text: 'Sua conta foi criada com sucesso',
-      timer: '3000'
-    }) 
-  } else {
-    Swal.fire({
-      icon: 'error',
-      title: 'Campos em branco',
-      text: 'Por favor, não deixe nenhum campo em branco!',
-    })  
   }
 });
