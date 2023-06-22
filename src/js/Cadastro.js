bbacafebc12147060e60b30b408a05fdb23deb41
Guilherme Simão Barbosa Da Silva <1310148@sga.pucminas.br>
2023-06-22 18:58:20 -0300

    Improved overall responsiveness

--- src/js/Cadastro.js
+++ src/js/Cadastro.js
@@ -1,3 +1,26 @@
 import Swal from "sweetalert2";
+import './ModuloConexao';
+import objConexao from "./ModuloConexao";
 
+// Inputs
+var inputNome = document.getElementById('inputNome');
+var inputEmail = document.getElementById('inputEmail');
+var inputContato = document.getElementById('inputNinputContato');
+var inputSenha = document.getElementById('inputSenha');
+var inputConfSenha = document.getElementById('inputConfSenha');
 
+// Limpar Campos Logic
+var limparCamposBtn = document.getElementById("limparCamposBtn");
+limparCamposBtn.addEventListener('click', function () {
+    inputNome = '';
+    inputEmail = '';
+    inputContato = '';
+    inputSenha = '';
+    inputConfSenha = '';
+});
+
+// Cadastrar Logic
+var criarContaBtn = document.getElementById('criarContaBtn');
+criarContaBtn.addEventListener('click', function () {
+    
+});
