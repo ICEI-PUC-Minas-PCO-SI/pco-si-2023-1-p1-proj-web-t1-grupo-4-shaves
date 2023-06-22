89423ef7572ea1cbd2a99a14c222c6fdf4b4e986
GuilhermeSimaos <guilhermesimaogh@outlook.com>
2023-06-22 17:19:10 -0300

    Worked on tablet responsiveness

--- src/js/Cadastro.js
+++ src/js/Cadastro.js
@@ -1,16 +1,3 @@
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
-// Mostrar Senha logic
-var mostrarSenhaCheckbox = document.getElementById("mostrarSenhaCheckbox");
-var inputSenha = document.getElementById("inputSenha");
-var inputConfSenha = document.getElementById("inputConfSenha");
 
-mostrarSenhaCheckbox.addEventListener("change", function () {
-  if (mostrarSenhaCheckbox.checked) {
-    inputSenha.type = "text";
-    inputConfSenha.type = "text";
-  } else {
-    inputSenha.type = "password";
-    inputConfSenha.type = "password";
-  }
-});
