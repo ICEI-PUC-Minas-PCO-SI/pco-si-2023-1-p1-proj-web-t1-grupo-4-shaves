2d040935084b1ecc780793b7f161942ff9231535
Felipe Castro <felipecacre@gmail.com>
2023-06-25 23:09:07 -0300

    Subindo função pesquisa barbeiro

---
+++ src/js/telabarbeiro.js
@@ -0,0 +1,37 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var searchBar = document.querySelector(".search-bar input");
+    var searchButton = document.querySelector(".search-bar button");
+    var cards = document.querySelectorAll(".card");
+
+    searchButton.addEventListener("click", function() {
+      var searchTerm = searchBar.value.trim().toLowerCase();
+      if (searchTerm !== "") {
+        cards.forEach(function(card) {
+          var cardTitle = card.querySelector(".card-title").textContent.toLowerCase();
+          if (cardTitle.includes(searchTerm)) {
+            card.style.display = "block";
+          } else {
+            card.style.display = "none";
+          }
+        });
+      } else {
+        cards.forEach(function(card) {
+          card.style.display = "block";
+        });
+      }
+    });
+
+    searchBar.addEventListener("input", function() {
+      var searchTerm = searchBar.value.trim().toLowerCase();
+      cards.forEach(function(card) {
+        var cardTitle = card.querySelector(".card-title").textContent.toLowerCase();
+        if (cardTitle.includes(searchTerm)) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    });
+  });
+
