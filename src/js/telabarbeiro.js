2ee269f3fb37a779765a1cce5c096c8cd5e148ce
Felipe Castro <felipecacre@gmail.com>
2023-07-05 22:01:00 -0300

    arruamando barra de pesquisa

--- src/js/telabarbeiro.js
+++ src/js/telabarbeiro.js
@@ -60,7 +60,7 @@ function filtraBarbeiros(todosUsuarios){
 
 $(document).ready(function() {
     
-    $('#search-input').on('input', function() {
+    $('.search-input').on('input', function() {
        
         var searchTerm = $(this).val().toLowerCase();
         
